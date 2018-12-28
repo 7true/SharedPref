888803cd6fd13e985f7441a69520c35ce953d210
alx <37true@gmail.com>
2018-12-28 07:32:42 +0700

    completed

--- app/src/main/java/tk/alltrue/sharedpref/MainActivity.java
+++ app/src/main/java/tk/alltrue/sharedpref/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int mCounter;
+    private TextView mInfoTextView;
 
     public static final String APP_PREFERENCES="mysettings";
     public static final String APP_PREFERENCES_COUNTER="counter";
@@ -16,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        mInfoTextView = (TextView) findViewById(R.id.textViewInfo);
+    }
+
+    public void onClick(View v) {
+        mInfoTextView.setText("I counted " + ++mCounter + " crows");
     }
 
     @Override
@@ -24,5 +33,15 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = mSettings.edit();
         editor.putInt(APP_PREFERENCES_COUNTER, mCounter);
         editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mSettings.contains(APP_PREFERENCES_COUNTER)) {
+           mCounter = mSettings.getInt(APP_PREFERENCES_COUNTER,0);
+           mInfoTextView.setText("I counted " + mCounter + " crows");
+        }
     }
 }
